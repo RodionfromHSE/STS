54aa08a4f0dd5835dd82bc3991e46354d556f4a1
RodionfromHSE <rodionkhvorostov@gmail.com>
2023-09-13 17:58:55 +0200

    Update configs

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,16 +2,19 @@ import json
 from omegaconf import OmegaConf, DictConfig
 
 
-def read_config(path: str) -> DictConfig:
+def read_config(path: str, set_args: dict = None) -> DictConfig:
     """Reads a config file from a given path and resolves it.
 
     Args:
         path (str): Path to the config file.
+        set_args (dict, optional): Arguments to set in the config file. Defaults to None.
 
     Returns:
         DictConfig: Config file in DictConfig format.
     """
     config = OmegaConf.load(path)
+    if set_args is not None:
+        config = OmegaConf.merge(config, set_args)
     config = OmegaConf.to_container(config, resolve=True)
     config = DictConfig(config)
     return config
